12b275152d2418199bffd5d6c799bfd0277fb2a7
Jason Kent <46462432+JasonK97@users.noreply.github.com>
2021-11-03 15:19:39 -0600

    Updated fatherly status

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="fade-in">
       <img class="meLogo" alt="Me logo" src="@/assets/me-bg.png" />
       <h1 class="title">Jason Kent</h1>
-      <p class="p-content">Web Developer, Husband, Disciple, Father-to-be.</p>
+      <p class="p-content">Web Developer, Husband, Disciple, Father.</p>
     </div>
 </template>
 
